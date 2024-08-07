f71be5b772dc482df8cb8db19fcda7c45f0f3827
ichimaru001 <ichimarusan345@proton.me>
2024-08-07 13:07:53 +0300

    Waiting to be deployed on Render

---
+++ middleware/authentication.js
@@ -0,0 +1,27 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const {UnathenticatedError} = require('../errors')
+
+const auth = (req,res,next)=>{
+  // check header
+  const authHeader = req.headers.authorization
+  if(!authHeader||!authHeader.startsWith('Bearer ')){
+    throw new UnathenticatedError('Authentication invalid')
+  }
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const payload = jwt.verify(token,process.env.JWT_SECRET)
+    // attach user to job routes
+
+    const user = User.findById(payload.id).select('-password')
+    req.user = user 
+
+    req.user = {userId:payload.userId,name:payload.name}
+    next()
+  } catch (error) {
+    throw new UnathenticatedError('Authentication invalid')
+  }
+}
+
+module.exports = auth
